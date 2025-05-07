629a2057059d7d02dd65585bef308cb4e1ed36d5
Adewole Adu <adewole.adu@l3harris.com>
2025-05-07 09:10:55 -0400

    add new articles from view and edit/update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,35 @@ class ArticlesController  < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #respond_to do |format|
+        #format.turbo_stream { render plain: params[:article] }
+        #end
+        if @article.save
+            flash[notice:] = 'Article was successfully created.'
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[notice:] = 'Article was successfully created.'
+            redirect_to @article
+        else
+            render :edit
+        end
+    end
+
 end
